ab3e2eca8078e6b115a43f5c193449670ebca094
raja-lingam <rajalingamsrrp@gmail.com>
2016-09-11 12:01:21 +0530

    Create ListOdd .java

---
+++ ListOdd .java
@@ -0,0 +1,21 @@
+import java.util.*;
+public class ListOdd {
+ 
+        public static void main(String[] args) {
+               
+                //define the limit
+                int l;
+                
+               Scanner in=new Scanner(System.in);
+               l=nextInt();
+                System.out.println("Printing Odd numbers between 1 and " + limit);
+               
+                for(int i=1; i <= l; i++){
+                       
+                        //if the number is not divisible by 2 then it is odd
+                        if( i % 2 != 0){
+                                System.out.print(i + " ");
+                        }
+                }
+        }
+}
